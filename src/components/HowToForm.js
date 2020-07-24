8fba609e5f1e54602f26d9f23491c153e28cfc9a
TonySo88 <60905365+TonySo88@users.noreply.github.com>
2020-07-24 13:26:45 -0500

    howtoform react 1 portion functionally complete

--- src/components/HowToForm.js
+++ src/components/HowToForm.js
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import * as yup from "yup";
+import Axios from "axios";
+
+const formSchema = yup.object().shape({
+  title: yup.string().required("Title is required"),
+  introduction: yup.string().required("Intro is required"),
+  steps: yup.string().required("Steps are required"),
+  tips: yup.string(),
+});
 
 const HowToForm = () => {
   const [howtoState, setHowtoState] = useState({
@@ -8,12 +17,43 @@ const HowToForm = () => {
     tips: "",
   });
 
+  const [errorState, setErrorState] = useState({
+    title: "",
+    introduction: "",
+    steps: "",
+    tips: "",
+  });
+
+  const validate = (event) => {
+    yup
+      .reach(formSchema, event.target.name)
+      .validate(event.target.value)
+      .then((valid) => {
+        setErrorState({
+          ...errorState,
+          [event.target.name]: "",
+        });
+      })
+      .catch((error) => {
+        console.log(error.errors);
+        setErrorState({
+          ...errorState,
+          [event.target.name]: error.errors[0],
+        });
+      });
+  };
+
   const formSubmit = (event) => {
     event.preventDefault();
+    Axios.post("https://reqres.in/api/users", howtoState)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   const inputChange = (event) => {
-    setHowtoState({ name: event.target.value });
+    event.persist();
+    validate(event);
+    setHowtoState({ ...howtoState, [event.target.name]: event.target.value });
   };
 
   return (
@@ -27,6 +67,7 @@ const HowToForm = () => {
           value={howtoState.title}
           onChange={inputChange}
         />
+        {errorState.title.length > 0 ? <p>{errorState.title}</p> : null}
       </label>
       <label htmlFor="introduction">
         Introduction - Share a quick intro with your DIYer
@@ -37,6 +78,9 @@ const HowToForm = () => {
           value={howtoState.introduction}
           onChange={inputChange}
         />
+        {errorState.introduction.length > 0 ? (
+          <p>{errorState.introduction}</p>
+        ) : null}
       </label>
       <label htmlFor="steps">
         Steps - Enter each step to your process here. Be as clear and
@@ -48,6 +92,7 @@ const HowToForm = () => {
           value={howtoState.steps}
           onChange={inputChange}
         />
+        {errorState.steps.length > 0 ? <p>{errorState.steps}</p> : null}
       </label>
       <label htmlFor="tips">
         Tips - Enter any tips, warnings, supplies needed, etc
